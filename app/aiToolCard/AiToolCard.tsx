3d686e5388627b67380632aa2056cb1c0353da8f
Ftamim003 <fazlullahtamim003@gmail.com>
2025-07-12 01:59:44 -0700

    ai tool

--- app/aiToolCard/AiToolCard.tsx
+++ app/aiToolCard/AiToolCard.tsx
@@ -1,7 +1,41 @@
 import React from 'react'
 
 import Image from 'next/image'
-import { aiToolData } from './aiToolData'
+
+
+export type AiToolType = {
+  id: number;
+  image: string;
+  
+  title: string;
+  description: string; // ✅ now it's just one paragraph
+};
+
+export const aiToolData: AiToolType[] = [
+  {
+    id: 1,
+    image: "/coinImage.png",
+    
+    title: "Pick a Tool That Pays",
+    description: " Browse our Tool Vault and choose ahigh-commission AI tttvvvool that fits niche.",
+  },
+  {
+    id: 2,
+    image: "/chain.png",
+    
+    title: "Copy Your Link & Share",
+    description: "  Use our build-in DM bot or TikTok templates to spread your link across social platforms.",
+  },
+  {
+    id: 3,
+    image: "/multiCoin.png",
+    
+    title: "Earn $1.77 + Every Time",
+    description: " You get paid whenever someone signs up through your link. it’s that simple.",
+  },
+
+];
+
 
 export default function AiToolCard() {
   return (

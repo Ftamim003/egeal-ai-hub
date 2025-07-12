165be25db083a7a527dcf83bb4f83189a3d264ba
Ftamim003 <fazlullahtamim003@gmail.com>
2025-07-11 21:18:55 -0700

    discover .

--- app/giveAways/GiveAways.tsx
+++ app/giveAways/GiveAways.tsx
@@ -1,9 +1,18 @@
+import Image from 'next/image'
 import React from 'react'
 
 export default function GiveAways() {
   return (
-    <div>
-        
+    <div className='max-w-[1079px] h-[529px]'>
+        <div>
+          <div>
+            <Image src='/imagePicture' alt='' width={67} height={66}></Image>
+              <div>
+                <h1>Giveaya 1</h1>
+              </div> 
+           </div>
+          
+        </div>
     </div>
   )
 }
